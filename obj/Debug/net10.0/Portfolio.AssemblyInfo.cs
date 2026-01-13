81fca8c07a570309ad76f5127cc391ef5abee5b6
weekijie <weekijie@gmail.com>
2026-01-14 02:07:39 +0800

    idk man git ignore stuff

--- obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
+++ obj/Debug/net10.0/Portfolio.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76292ee467c485465b5973b5762404c4794c5407")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+850d48437353e4857fb0c0b59d910b35b7352142")]
 [assembly: System.Reflection.AssemblyProductAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Portfolio")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
